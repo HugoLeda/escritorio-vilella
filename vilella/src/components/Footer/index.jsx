747eb266974e7b94e68600d1b8fa89a67fd2b1da
Escritório Vilella <suportevilella@gmail.com>
2024-04-17 16:27:31 -0300

    html-footer

--- vilella/src/components/Footer/index.jsx
+++ vilella/src/components/Footer/index.jsx
@@ -1,20 +1,32 @@
-import { FaWhatsapp } from "react-icons/fa";
+import { MdOutlineWhatsapp, MdLocalPhone, MdMail } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <footer>
       <div>
-        Fale Conosco
-        <FaWhatsapp />
+        <h2>Fale Conosco</h2>
+        <p><MdLocalPhone />(14) 3382-2052</p>
+        <p><MdOutlineWhatsapp />(14) 99779-5080</p>
+        <p><MdMail />admin@evilella.com</p>
+        <h2>Redes Sociais</h2>
+        <p><FaInstagram />escritoriovilella</p>
       </div>
       <div>
-        Nossos Serviços
+        <h2>Nossos Serviços</h2>
         <ul>
           <li>Certificadora</li>
+          <li>Soluções Contábeis</li>
+          <li>Soluções Fiscais</li>
+          <li>Pessoa Física</li>
+          <li>Empresarial</li>
+          <li>MEI</li>
+          <li>Rural</li>
         </ul>
       </div>
       <div>
-        Portal
+        <img src="src\assets\logo.png" alt="Vilella"/>
+        Portal do Cliente
       </div>
     </footer>
   )
