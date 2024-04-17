e23eb51c5c6b2287f40e08f90029496ffba3e0b5
João Hugo Leda <74215171+HugoLeda@users.noreply.github.com>
2024-04-17 13:36:43 -0300

    add libraries

---
+++ vilella/src/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        Fale Conosco
+        <FaWhatsapp />
+      </div>
+      <div>
+        Nossos Serviços
+        <ul>
+          <li>Certificadora</li>
+        </ul>
+      </div>
+      <div>
+        Portal
+      </div>
+    </footer>
+  )
+}
