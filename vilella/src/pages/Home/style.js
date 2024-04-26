5430712002047071bafb6d7f8cda288b7a255878
Escritório Vilella <suportevilella@gmail.com>
2024-04-26 16:21:15 -0300

    home-content

--- vilella/src/pages/Home/style.js
+++ vilella/src/pages/Home/style.js
@@ -1,4 +1,84 @@
 import styled from "styled-components";
 
-export const Homepage = styled.div `
+export const Homepage = styled.div `      
+/*  .flex {
+    display: flex;
+  }
+
+  .flex-column {
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1, h2 {
+    font-size: 50px;
+    margin-bottom: 10px;
+  }
+  
+  h3 {
+    font-size: 30px;
+    margin-bottom: 5px;
+  }
+
+  p {
+      margin-bottom: 20px;
+  }
+  
+  .bsc-boxes {
+    display: flex;
+    justify-content: center;
+    gap: 100px;
+    
+    text-align: center;
+    
+    .bsc-boxes-vision {
+      width: 300px;
+      height: 300px;
+      border-radius: 3px;
+      transition: scale 0.5 ease;
+      box-shadow: 0 0 10px rgb(0,0,0,0.3);
+    }
+
+    .bsc-boxes-vision:hover {
+      transform: scale(1.02);
+    }
+    
+    .bsc-boxes-values{
+      //width: 300px;
+      //height: 300px;
+      border-radius: 3px;
+      transition: scale 0.5 ease;
+      box-shadow: 0 0 10px rgb(0,0,0,0.3);
+    }
+    .bsc-boxes-values:hover {
+      transform: scale(1.02);
+    }
+  }
+
+  .history {
+    .history-text-numbers {
+      span {
+         font-weight: bold;
+      }
+    }
+  }
+
+  .solution-icons {
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;    
+  }
+
+  /*.general-contact {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom:30px;
+
+    .map-address {
+      width: 100px;
+      height: 100px;
+      box-shadow: 0 0 3px;
+    }
+  }
+*/
 `
