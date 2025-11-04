ef1dc4ffb97c301caae40d768127ceec5a8d6974
hugoleda <joaohugoleda2@gmail.com>
2025-11-04 10:53:01 -0300

    rebuild Banner component full layout and content

--- vilella/src/components/Banner/index.jsx
+++ vilella/src/components/Banner/index.jsx
@@ -1,43 +1,79 @@
 import { BannerContainer } from "./style";
-import 'bootstrap/dist/css/bootstrap.css';
-import Carousel from 'react-bootstrap/Carousel';
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
 
-import homeCover from '../../assets/homeCover.webp'
-import darkBlueBG from '../../assets/blueVilella.webp'
-import { Link } from 'react-router-dom';
+import entrance from "../../assets/entrance.webp";
+import team from "../../assets/team.png";
 
 export default function Banner() {
   return (
     <BannerContainer>
-      <div className="cover">
-        <Carousel >
-          <Carousel.Item interval={30000}>
-            <img className="cover-img" 
-                 src={homeCover} alt="Foto da fachada do Escritório Vilella"
-            />
-            <Carousel.Caption className="st-caption display">
-              <h3>Contabilidade de Confiança</h3>
-              <p>Descubra como podemos ajudar você a prosperar financeiramente.</p>
-              <button className="more-aboutus">
-                <Link to={"/services"}> Saiba Mais </Link>
-              </button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={30000}>
-            <img className="cover-img" 
-                 src={darkBlueBG} alt="Imagem com a logo do Escritório Vilella"
-            />
-            <Carousel.Caption className="nd-caption display">
-              <h3>Expertise e Experiência</h3>
-              <p>Temos o conhecimento necessário para oferecer o melhor suporte possível.</p>
-              <button className="more-services">
-                <Link to={"/team"}> Saiba Mais </Link>
-              </button>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
+      <Carousel fade interval={30000}>
+        
+        {/* ======= ITEM 1 ======= */}
+        <Carousel.Item>
+          <section id="banner" className="banner-office">
+            <div>
+
+            <div className="banner flex wrapper office">
+              <div className="banner-text flex-column">
+                <div className="text flex">
+                  <h2>Escritório Vilella</h2>
+                  <div className="main-title flex-column">
+                    <h1>Do registro do CNPJ à contabilidade completa</h1>
+                    <p>Estamos com você  <span> em cada etapa</span></p>
+                  </div>                  
+                </div>
+                <div className="button flex">
+                  <button className="specialists">
+                    <a href="https://wa.me/5514997795080" target="blank">Fale com nossos especialistas</a>
+                  </button>
+                  <button className="services">
+                    <a href="/services">Ver serviços</a>
+                  </button>
+                </div>
+              </div>              
+              <div className="image flex">
+                <img src={entrance} alt="Fachada do Escritório Vilella" />
+              </div>
+            </div>
+
+            </div>
+          </section>
+        </Carousel.Item>
+
+        {/* ======= ITEM 2 ======= */}
+        <Carousel.Item interval={30000}>
+          <section id="banner" className="banner-team">
+            <div>
+              <div className="banner flex wrapper team">
+              <div className="banner-text flex-column">
+                <div className="text flex">
+                  <div className="banner-team-title flex">
+                    <h1><span className="years">35</span> anos de <br /> excelência</h1>
+                  </div>                  
+                  <p>Há 35 anos, nossa excelência é construída por pessoas que fazem a diferença todos os dias.</p>
+                </div>
+                <div className="button flex">
+                  <button className="our-team">
+                    <a href="/team">Veja quem faz acontecer</a>
+                  </button>
+                  <button className="work-with-us">
+                    <a href="mailto:contato@evilella.com?subject=Curr%C3%ADculo%20-%20Trabalhe%20Conosco&body=Olá,%20gostaria%20de%20enviar%20meu%20currículo%20para%20futuras%20oportunidades.">Trabalhe Conosco</a>
+
+                  </button>
+                </div>
+              </div>   
+              <div className="image">
+                <img src={team} alt="Fachada do Escritório Vilella" />
+              </div>
+            </div>
+            </div>
+            
+          </section>
+        </Carousel.Item>
+      </Carousel>
     </BannerContainer>
-  )
+  );
 }
-  
--- vilella/src/components/Banner/style.js
+++ vilella/src/components/Banner/style.js
@@ -1,112 +1,596 @@
 import styled from 'styled-components'
-import media from "styled-media-query"
 import Theme from '../../styles/theme'
 
 export const BannerContainer = styled.section`
 
-  max-width: 1000vw;
-  user-select: none;
+  /*for both banners */
 
-  h3 {
-    font-size: ${Theme.textSize.titleMobileLg};
-    font-weight: 600;
+  #banner {
+    user-select: none;
   }
 
-  p {
-    font-size: ${Theme.textSize.textMobileLg};
-    font-weight: 500;
-  }
-
-  .cover {
+  .flex,
+  .flex-column {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;    
-  }
-
-  .cover-img, img {
-    width: 100%;    
-    height: calc(100vh - 2.4rem - 67px);
-    object-fit: cover;
-  }
-
-
-  .st-caption {
-    display: none;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-
-    padding: 2rem;
-
-    border-radius: 3px;
-    background-color: rgba(147, 198, 255, 0.5);
-    box-shadow: 0 0 8px rgb(0,0,0,0.2);
-
-    h3 { 
-      color: ${Theme.colors.blue1}; 
-    }
   }
   
-  .nd-caption {
-    display: none;
+  .flex-column {
     flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    
-    padding: 2rem;
-    
-    h3 { 
-      color: ${Theme.colors.defwhite}; 
-    }    
   }
 
-  button {
-    width: 130px;
-    height: 40px;
-    border-radius: 6px;
-    box-shadow: 0 2px 8px rgb(0,0,0,0.5);
-    border: none;
+  .wrapper {
+    max-width: 1920px;
+    margin: auto;
+    padding: 8rem 10%;
+  }
+  
+  .banner-text {
+    gap: 3rem;
+  }
 
-    background-color: ${Theme.colors.blue1};
-    color: white;
-        
-    cursor: pointer;
-    transition: ease-out all .3s;
+  .text {    
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+    max-width: 600px;
+    height: 100%;
 
-    a {
-      font-size: ${Theme.textSize.textSm};
-      font-weight: 700;
-
-      color: ${Theme.colors.white};
-      text-decoration: none;
-    }
-
-    &:hover {
-      background-color: ${Theme.colors.babyBlue};
-      color: ${Theme.colors.blue1};
-      a {
-        color: ${Theme.colors.blue1};
+    p {
+      font-size: ${Theme.textSize.titleCardSm};
+      font-weight: 400;
+      position: relative;
+      
+      span {
+        text-transform: none;
+        padding: 0;
+        color: ${Theme.colors.blue3};
       }
     }
   }
 
-  ${media.lessThan("large")`  
-    margin-bottom: 2rem;
-    .cover-img, img {
-      height: calc(100vh - .5rem - 67px);
+  
+  button {
+    padding: .2rem 1rem;
+    height: 45px;
+    border: none;    
+    transition: ease-out all .2s;
+    background-color: transparent;
+
+    a {
+      text-decoration: none;      
+      font-weight: 400;
+      text-transform: uppercase;
+    }
+  }
+
+  
+  .button {
+    gap: 1rem;
+    align-items: flex-end;
+    //height: 100%;
+  }
+
+  
+  .image {
+    display: flex;
+    align-items: center;
+    border-radius: 40px;
+    position: relative;
+
+    img {
+      width: 350px;      
+      height: 350px;
+      object-fit: cover;
+      border-radius: 40px;
+      object-position: 0% 50%;
+    }
+  }
+
+
+
+  /*office banner  */
+
+  .office {
+    justify-content: center;
+    gap: 5rem;
+  }
+
+  .banner-office {
+    gap: 5rem;
+    background-color: #EFF6FF;
+
+    .text {
+      p {
+        color:${Theme.colors.blue};
+        
+        span {
+          text-transform: none;
+          padding: 0;
+          color: ${Theme.colors.blue3};
+        }
+
+        &::before {
+          position: absolute;
+          content: " ";
+          background: linear-gradient(to right, #93c5ff, #eff6ff, #eff6ff);
+          width: 100%;
+          height: 2px;
+          bottom: -1.5rem;
+          border-radius: 40px;
+        }
+      }
+
+      span {
+        text-transform: uppercase;  
+        text-align: center;
+        font-weight: 500;
+        border-radius: 40px;          
+        padding: .2rem .5rem;
+      }
+    }
+
+    h1 {
+      font-size: ${Theme.textSize.titleClientXLg};
+    }
+
+    h1,h2 {    
+      color: ${Theme.colors.blue};
+    }
+
+
+    .specialists,
+    .services {
+      border-radius: 8px;
     }
     
-    .display {
+    .specialists {
+      background-color: #0d2daa;
+      a {
+        color: ${Theme.colors.white};
+      }    
+      &:hover {            
+        background-color: ${Theme.colors.babyBlue};
+      a {
+        color: ${Theme.colors.blue};
+      }    
+      }
+    }
+    
+    .services {
+      border: 1px solid #0d2daa;
       background-color: transparent;
-      display: none;    
+      a {
+        color: ${Theme.colors.blue};
+      }    
+      &:hover {            
+        background-color: ${Theme.colors.babyBlue};
+        border: 1px solid ${Theme.colors.babyBlue};
+      a {
+        color: ${Theme.colors.white};
+      }    
+      }
     }
- `}   
 
-  ${media.lessThan("medium")`
-    .cover-img, img {              
-      height: auto;
-      object-fit: fill;
+
+    .image {
+      &::after {
+        content: "";
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        background-color: ${Theme.colors.babyBlue};
+        border-radius: 1rem;
+        left: 0rem;
+        bottom: -3rem;
+        box-shadow: -3rem -3rem 0 0 ${Theme.colors.blue1};      
+      }
+
+      &::before {
+        content: "";
+        position: absolute;
+        width: 50px;
+        height: 50px;
+        background-color: ${Theme.colors.blue2};
+        border-radius: .75rem;
+        right: 2rem;
+        top: -1.5rem;    
+      }
     }
-  `}    
+  }
+
+
+  /* team banner */
+
+  
+  .team {
+    flex-direction: row-reverse;
+    justify-content: center;
+    gap: 5rem;
+  }
+
+  .banner-team {
+    background: linear-gradient(to right, #00116f, #0d2daa);
+    
+    .text {
+      gap: 0;
+      p {
+        color: ${Theme.colors.white};
+        
+        &::before {
+          position: absolute;
+          content: " ";
+          background: linear-gradient(to right, #93c5ff, #0d2daa);
+          width: 100%;
+          height: 2px;
+          bottom: -1.5rem;
+          border-radius: 40px;
+        }
+      }
+    }
+
+    .image {        
+      display: flex;
+      align-items: center;
+      img {
+        object-position: 40% 50%;
+      }
+
+      &::after {
+        content: "";
+        position: absolute;
+        width: 75px;
+        height: 75px;
+        background-color: ${Theme.colors.babyBlue};
+        border-radius: 1rem;
+        left: -2rem;
+        bottom: 18rem;
+        box-shadow: 2rem -2rem 0 0 ${Theme.colors.white};      
+      }
+
+      &::before {
+        content: "";
+        position: absolute;
+        width: 50px;
+        height: 50px;
+        background-color: ${Theme.colors.blue};
+        border-radius: .75rem;
+        right: 2rem;
+        top: 20rem;    
+      }
+    }
+
+    h1 {
+      font-size: ${Theme.textSize.titleClientXLg};
+      display: flex;
+      width: 75%;
+      align-items: center;
+      gap: 1rem;
+      .years {
+        font-size: 8rem;
+        font-weight: 400;
+      }
+    }
+
+    h1,h2 {
+      color: ${Theme.colors.white};
+    }
+
+    .our-team,
+    .work-with-us {
+      border-radius: 8px;
+    }
+    
+    .our-team {
+      border: 1px solid ${Theme.colors.white};
+      background-color: transparent;
+      a {
+        color: ${Theme.colors.white};
+      }    
+      &:hover {            
+        background-color: ${Theme.colors.babyBlue};
+        border: 1px solid ${Theme.colors.babyBlue};
+      a {
+        color: ${Theme.colors.white};
+      }    
+      }
+    }
+    
+    .work-with-us {
+      border: 1px solid #f33;
+      background-color: transparent;
+      a {
+        color: ${Theme.colors.red2};
+      }    
+      &:hover {            
+        background-color: ${Theme.colors.babyBlue};
+        border: 1px solid ${Theme.colors.babyBlue};
+      a {
+        color: ${Theme.colors.white};
+      }    
+      }
+    }
+
+  }
+
+  
+
+
+  @media (max-width: 1180px) {
+    
+    #banner {
+      min-height: 600px;
+    }
+
+    .image {
+      img {
+      width: 300px;
+      height: 300px;
+    }
+  }
+
+    .office {
+      gap: 0;
+    }
+
+    .banner-office {
+      
+      .image {
+        &::after {
+          width: 75px;
+          height: 75px;
+          left: 1rem;
+          bottom: -2rem;
+          box-shadow: -3rem -3rem 0 0 ${Theme.colors.blue1};      
+        }
+
+        &::before {
+          border-radius: .75rem;
+          right: 2rem;
+          top: 0;    
+        }
+      }
+    }
+    
+    .team {
+      gap: 2rem;
+    }
+
+    .banner-team {
+        
+      .image {
+        img {
+          object-position: 40% 50%;
+        }
+
+        &::after {
+          left: -2rem;
+          bottom: 15rem;    
+        }
+
+        &::before {
+          right: 2rem;
+          top: 18.5rem;    
+        }
+      }
+    }
+  }
+  
+  @media (max-width: 1080px) {
+  
+    .banner {
+        gap: 3rem;
+      }
+
+    .image {
+      img {
+        width: 275px;
+        height: 275px;
+      }
+
+    }
+
+    h1 {
+      font-size: ${Theme.textSize.titleMd};
+    }
+     
+    .text {
+      p {
+        font-size: ${Theme.textSize.textMd};
+      }
+    }
+
+    .banner-team {
+      .image {
+        &::before {
+          right: 2rem;
+          top: 18rem;    
+        }
+      }
+    }
+
+  }
+
+  
+  @media (max-width: 1024px) {
+
+    
+    #banner,
+    .banner-office,
+    .banner-team {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      min-height: 600px;
+    }
+
+    
+  .wrapper {
+    width: 100%;
+    max-width: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .banner-text {
+    width: 100%;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: left;
+  }
+
+    .banner-office .image,
+    .banner-team .image {
+      display: none;
+    } 
+      
+    .banner-text {
+      gap: 2rem;
+    }
+
+    .banner-office {
+      .main-title {
+        gap: 1rem;
+      }
+      p {
+        font-size: ${Theme.textSize.textLg};
+        &::before {          
+          background: linear-gradient(to right, #93c5ff, #eff6ff);
+        }
+      }
+
+    }
+
+    .button {
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+      
+      .specialists,
+      .services {
+        width: 100%;
+      }
+    }
+
+    .team {
+      flex-direction: column-reverse;
+    }
+  
+    .banner-team {
+
+      .main-title {
+        gap: 1rem;
+      }
+
+      button {
+        width: 100%;
+        max-width: 600px;
+      }
+
+    }
+
+  }
+
+  @media (max-width: 790px) {
+    
+    .wrapper {
+      padding: 5% 10%;
+    }
+
+    .text {
+      max-height: 500px;
+    }
+
+    .banner-office {
+      h1 {        
+        font-size: ${Theme.textSize.titleClientMd};
+      }
+    }
+    .banner-team {
+      align-items: center;
+
+      .text {
+        justify-content: center;
+        gap: 2rem;
+      }
+
+
+      .banner-text {
+        height: 100%;
+        position: relative;
+      
+        &::before {
+          position: absolute;
+          content: " ";
+          background: linear-gradient(to right, #00116f, #93c5ff, #0d2daa);
+          width: 75%;
+          height: 2px;
+          bottom: 35%;
+          border-radius: 40px;
+
+        }
+      }
+
+      .banner-team-title {
+        justify-content: center;
+      }
+
+      h1 {
+        font-size: ${Theme.textSize.titleClientSm};
+        width: 100%;
+        justify-content: center;
+        
+        .years {
+          font-size: 6rem;
+        }
+      }
+
+      p {
+        text-align: center;
+        font-size: ${Theme.textSize.textLg};
+        &::before {          
+          display: none;    
+        }
+      }
+    }
+
+    .wrapper {
+      height: calc(80vh - 2.4rem - 67px);
+    }
+
+  }
+
+  @media (max-width: 390px) {
+    
+    .banner-office {
+
+      button {
+        padding: 0;
+        a {
+          font-size: .9rem;
+        }
+      }
+      .text {
+        h1 {
+          font-size: ${Theme.textSize.titleMobileMd};
+        }
+        p {
+          font-size: ${Theme.textSize.textMobileLg};
+          &::before {
+            //display: none;
+          }
+        }
+      }
+    }
+
+    .banner-team {
+      h1 {
+        gap: .5rem;
+      }
+    }
+  }
+
 `
